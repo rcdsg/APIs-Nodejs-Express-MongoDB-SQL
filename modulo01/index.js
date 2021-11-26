4a675a3e0b3f204989f1dab439b86c79157a1b4c
Rodrigo Costa <rodrigokosta@gmail.com>
2021-11-26 20:22:43 -0300

    adding crud methods

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,9 +2,58 @@ const express = require('express');
 
 const server = express();
 
-server.get('/curso', (req, res) => {
-    //return res.send('heloo wodlldld!');
-    return res.json({curso: 'sddsad'})
+server.use(express.json());
+/*
+Query Params = ?nome=Rodrigo&idade=32
+Route Params = /curso
+Request Body = { nome: 'Rodrigo', cargo: 'Front-End'}
+*/
+
+/*
+CRUD = Create, Read, Update, Delete
+*/
+const cursos = ['Node JS', 'JavaScript', 'React Native'];
+
+server.get('/cursos', (req, res)=> {
+    return res.json(cursos);
+});
+
+server.get('/cursos/:index', (req, res) => {
+/*
+    return res.send('Hello World!');
+    const nome = req.query.nome;
+    const cargo = req.query.cargo;
+    const id = req.params.id;
+*/
+    const { index } = req.params;
+    return res.json(cursos[index])
+    //return res.json({curso: `Curso de Node js ID: ${id}. Aluno: ${nome} Cargo: ${cargo}`})
+})
+
+//Criando um novo curso
+server.post('/cursos', (req, res)=> {
+    const { name } = req.body;
+    cursos.push(name);
+  
+    return res.json(cursos);
+});
+
+//Atualizando um curso
+server.put('/cursos/:index', (req,res) => {
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+//Deletando um curso
+server.delete('/cursos/:index', (req,res) => {
+    const {index} = req.params;
+
+    cursos.splice(index,1);
+    return res.json(cursos);
 })
 
 server.listen(3000);
